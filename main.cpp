6dcdeed280b20c74755fe5e00afa7ee321dd037b
sigmasleep <sarkar.krishnan@gmail.com>
2017-01-27 02:49:14 -0500

    Add Window

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -5,6 +7,34 @@
 
 int main()
 {
-	//basic compilation test
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(640,480, "Duckbut", NULL, NULL);
+  if(!window)
+  {
+    printf("GLFW window creation failure.\n");
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (glewInit() != GLEW_OK)
+    {
+      printf("GLEW initialization failure.\n");
+      glfwTerminate();
+      return -1;
+    }
+
+  while(!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+  }
+  
+  glfwTerminate();
+
 	return 0;
 }
