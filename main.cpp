c50fd270c73dbbebc3d4115282ccf875fce4ee0b
sigmasleep <sarkar.krishnan@gmail.com>
2017-01-27 23:10:44 -0500

    Render Rectangle

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@ int main()
 
   glfwMakeContextCurrent(window);
 
+  glewExperimental = GL_TRUE;
   if (glewInit() != GLEW_OK)
     {
       printf("GLEW initialization failure.\n");
@@ -29,11 +30,102 @@ int main()
       return -1;
     }
 
+  int width, height;
+  glfwGetFramebufferSize(window, &width, &height);
+  glViewport(0, 0, width, height);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+  GLchar* vertexShaderSource =
+    "#version 400 core\n"
+    "layout (location = 0) in vec3 position;"
+    "void main()"
+    "{"
+    " gl_Position = vec4(position.x, position.y, position.z, 1.0);"
+    "}";
+
+  GLchar* fragmentShaderSource =
+    "#version 400 core\n"
+    "out vec4 color;"
+    "void main()"
+    "{"
+    " color = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+    "}";
+
+  GLuint vertexShaderID;
+  vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShaderID, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShaderID);
+
+  GLuint fragmentShaderID;
+  fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShaderID, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShaderID);
+
+  GLuint shaderProgramID = glCreateProgram();
+  glAttachShader(shaderProgramID, vertexShaderID);
+  glAttachShader(shaderProgramID, fragmentShaderID);
+  glLinkProgram(shaderProgramID);
+
+  GLint success;
+  glGetShaderiv(fragmentShaderID, GL_COMPILE_STATUS, &success);
+  if(!success)
+  {
+    printf("failure");
+  }
+
+  glGetShaderiv(vertexShaderID, GL_COMPILE_STATUS, &success);
+  if(!success)
+  {
+    printf("failure");
+  }
+
+  //glDeleteShader(vertexShaderID);
+  //glDeleteShader(fragmentShaderID);
+
+  GLfloat squareVertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    -0.5f,  0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.5f,  0.5f, 0.0f
+  };
+
+  GLuint squareIndices[] = {
+    0, 1, 2,
+    2, 1, 3
+  };
+    
+  GLuint VBO, VAO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+  {
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(squareVertices), squareVertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(squareIndices), squareIndices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+  }
+
   while(!glfwWindowShouldClose(window))
   {
     glfwPollEvents();
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgramID);
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+
+    glfwSwapBuffers(window);
   }
-  
+
   glfwTerminate();
 
 	return 0;
